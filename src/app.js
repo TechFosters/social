bde5ac39b2237c403e2c5bfa9763e33c6d605913
TechFosters <akkihere8@gmail.com>
2025-07-18 22:05:40 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -2,14 +2,21 @@ import express from 'express'
 
 const app = express();
 
-//request handler
-app.get("/",(req, res)=>{
+//request handler: it handles all the request starting with / ex: /hello, /xyz
+
+app.use("/",(req, res)=>{
     res.send("this is the dashboard")
 })
-//request handler via route
+//request handler via route : it handles att the request starting witj /hello ex /hello/v1
 app.use("/hello", (req,res)=>{
     res.send("Hello, Helloo, Helllooo")
 })
+
+app.use("/test", (req,res)=>{
+    res.send("test, test, test")
+})
+
+
 //creating the server
 app.listen(3000, ()=>{
     console.log('server is running at port 3000')
