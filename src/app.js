06018d37aa7f914d753359ac7e04bd0e148172d7
TechFosters <akkihere8@gmail.com>
2025-07-19 10:31:06 +0530

    regex routes, query params, dynamic routes

--- src/app.js
+++ src/app.js
@@ -2,35 +2,64 @@ import express from 'express'
 
 const app = express();
 
-//order of the route matters
-// app.use("/user",(req,res)=>{
-//     res.send({
-//         success: true,
-//         message: "this is app.use()"
-//     })
-// })
+//? means b is optional it works for /abc and /ac
+app.get(/^\/a(b)?c$/, (req, res) => {
+    res.send({ message: "this is a(b)?c" });
+});
 
-//GET REQUEST
+// q+ means any number of qs between p and r
+app.get(/^\/pq+r$/,(req,res)=>{
+    res.send({message: "this is for pq+r"})
+})
+
+// anything (any characters) can come between he and ld.
+app.get(/^\/he.*ld$/,(req,res)=>{
+    res.send({message: "this is for he*ld"})
+})
+
+app.get(/a/, (req,res)=>{
+    res.send({message: "this is for/a/ that is it workd for any route that contains a"})
+})
+
+app.get(/.*dev$/, (req,res)=>{
+    res.send({message: "this is for anything that ends with dev"})
+})
+
+/*dynamic route
+:id and :name are URL segments, treated as variables.
+You access them via req.params.
+
+💁🏻Example:
+GET http://localhost:3000/user/42/akshit
+
+✅req.params will log:
+{ id: "42", name: "akshit" }
+ */
+
+app.get("/user/:id/:name", (req,res)=>{
+    console.log(req.params)
+    res.send({message: "this is for user/:id"})
+})
+
+
+
+/* how to read query params:
+These are extra data sent with the URL, usually for filtering, searching, sorting, etc.
+You access them using req.query 
+
+💁🏻Example:
+GET http://localhost:3000/user?age=25&city=delhi
+
+✅req.query will log:{ age: "25", city: "delhi" }
+*/
 app.get("/user", (req, res)=>{
+    console.log(req.query)
     res.send({
         sucess: true,
         message: "this is a GET request"
     })
 })
-//POST REQUEST
-app.post("/user",(req, res)=>{
-   res.send({
-    sucess: true,
-    message: "this is a POST request"
-   })
-})
-//DELETE REQUEST
-app.delete("/user",(req, res)=>{
-    res.send({
-        success: true,
-        message: "this is a DELETE request"
-    })
-})
+
 
 //creating the server
 app.listen(3000, ()=>{
