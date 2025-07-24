8064cde9980ef0c086a0fbb450e24bfe522904e4
TechFosters <akkihere8@gmail.com>
2025-07-24 17:01:07 +0530

    what next does

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ import express from 'express'
 
 const app = express();
 
-app.use('/user', (req, res)=>{
+//req,res, next is given to us by express they are parameter to express route handler
+app.use('/user', (req, res,next)=>{
     // console.log(req.body)
     res.send("Hello from route handler1") //as this runs
-    console.log(req)
+    console.log("Hello from route handler1") //logs
+    next(); //this will pass the flow to next request
 },
 (req,res)=>{
-    res.send("Hello from route handler2") //this won't be processed automatically 
+    console.log("Hello from route handler2") //logs
+    res.send("Hello from route handler2") //since the response is already sent earlier from route handler1 it won't work: Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
 
 })
 
